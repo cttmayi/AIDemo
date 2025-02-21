242370f5028670bebe1603aaa88c3bc2c6117481
ling.yuan <cttmayi@hotmail.com>
2025-02-21 23:05:37 +0800

    update

--- fine-tune/deepseek_r1/v3_u.py
+++ fine-tune/deepseek_r1/v3_u.py
@@ -45,7 +45,7 @@ model, tokenizer = FastLanguageModel.from_pretrained(
     model_name = model_name,
     max_seq_length = max_seq_length,
     load_in_4bit = True, # False for LoRA 16bit
-    fast_inference = True, # Enable vLLM fast inference
+    # fast_inference = True, # Enable vLLM fast inference
     max_lora_rank = lora_rank,
     gpu_memory_utilization = 0.6, # Reduce if out of memory
 )
