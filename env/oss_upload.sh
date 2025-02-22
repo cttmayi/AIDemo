32b7b80a32a6fe9426d6fc23dbe091e5761a7214
ling.yuan <cttmayi@hotmail.com>
2025-02-23 00:09:00 +0800

    update

--- env/oss_upload.sh
+++ env/oss_upload.sh
@@ -2,31 +2,33 @@
 
 oss login
 
-oss mkdir oss://backup/
+oss mkdir oss://hy-tmp/
 
-if [ -d "/hy-tmp/models" ]; then
-    tar -zcf /hy-tmp/models.tar.gz /hy-tmp/models
-    oss cp /hy-tmp/models.tar.gz oss://backup/
+cd /hy-tmp || exit
+
+if [ -d "models" ]; then
+    tar -zcf models.tar.gz models
+    oss cp models.tar.gz oss://hy-tmp/
     if [ $? -eq 0 ]; then
-        # rm -rf /hy-tmp/models
-        rm /hy-tmp/models.tar.gz
+        # rm -rf models
+        rm models.tar.gz
     fi
 fi
 
-if [ -d "/hy-tmp/datasets" ]; then
-    tar -zcf /hy-tmp/datasets.tar.gz /hy-tmp/datasets
-    oss cp /hy-tmp/datasets.tar.gz oss://backup/
+if [ -d "datasets" ]; then
+    tar -zcf datasets.tar.gz datasets
+    oss cp datasets.tar.gz oss://hy-tmp/
     if [ $? -eq 0 ]; then
-        # rm -rf /hy-tmp/datasets
-        rm /hy-tmp/datasets.tar.gz
+        # rm -rf datasets
+        rm datasets.tar.gz
     fi
 fi
 
-if [ -d "/hy-tmp/outputs" ]; then
-    tar -zcf /hy-tmp/outputs.tar.gz /hy-tmp/outputs
-    oss cp /hy-tmp/outputs.tar.gz oss://backup/
+if [ -d "outputs" ]; then
+    tar -zcf outputs.tar.gz outputs
+    oss cp outputs.tar.gz oss://hy-tmp/
     if [ $? -eq 0 ]; then
-        # rm -rf /hy-tmp/outputs
-        rm /hy-tmp/outputs.tar.gz
+        # rm -rf outputs
+        rm outputs.tar.gz
     fi
 fi
