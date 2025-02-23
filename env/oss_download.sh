c5b91d7bfc279ba5ef945a403f3fc0df81683f1b
ling.yuan <cttmayi@hotmail.com>
2025-02-23 22:13:32 +0800

    update

--- env/oss_download.sh
+++ env/oss_download.sh
@@ -7,11 +7,20 @@ oss login
 
 cd ~/cache || exit
 
-folder="outputs"
+# 定义函数
+copy() {
+    local folder="$1"  # 使用局部变量接收传入的参数
+    local file="${folder}.zip"
 
-file="${folder}.zip"
-oss cp "oss://hy-tmp/${file}" .
-if [ -f "$file" ]; then
-    unzip -q "$file"
-    rm "$file"
-fi
+    # 从 OSS 下载文件
+    oss cp "oss://hy-tmp/${file}" .
+    if [ -f "$file" ]; then
+        unzip -q "$file"  # 解压文件
+        rm "$file"        # 删除下载的 zip 文件
+    else
+        echo "Error: File $file does not exist after download."
+        return 1  # 返回错误状态码
+    fi
+}
+
+copy "outputs"
