a5c64ea40f24fe6bca3b117c077a3bbfd10bbbbe
ling.yuan <cttmayi@hotmail.com>
2025-02-23 21:55:11 +0800

    update

--- env/oss_upload.sh
+++ env/oss_upload.sh
@@ -4,11 +4,13 @@ oss login
 
 oss mkdir oss://hy-tmp/
 
-cd /hy-tmp || exit
+cd ~/cache || exit
 
+folder="outputs"
 
 if [ -d "outputs" ]; then
-    file="result-$(date "+%Y%m%d-%H%M%S").zip"
+    # file="result-$(date "+%Y%m%d-%H%M%S").zip"
+    file="${folder}.zip"
     zip -q -r "${file}" outputs
     # tar -zcf outputs.tar.gz outputs
     oss cp $file oss://hy-tmp/
