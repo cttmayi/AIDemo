bafc68c590d8374154d952102370f585999e8236
ling.yuan <cttmayi@hotmail.com>
2025-02-23 22:16:39 +0800

    update

--- env/oss_upload.sh
+++ env/oss_upload.sh
@@ -9,17 +9,36 @@ oss mkdir oss://hy-tmp/
 
 cd ~/cache || exit
 
-folder="outputs"
+# 定义函数
+upload() {
+    local folder="$1"  # 使用局部变量接收传入的参数
 
-if [ -d "outputs" ]; then
-    # file="result-$(date "+%Y%m%d-%H%M%S").zip"
-    file="${folder}.zip"
-    zip -q -r "${file}" outputs
-    # tar -zcf outputs.tar.gz outputs
-    oss cp $file oss://hy-tmp/
-    if [ $? -eq 0 ]; then
-        rm -f $file
+    # 检查文件夹是否存在
+    if [ -d "$folder" ]; then
+        local file="${folder}.zip"  # 定义压缩文件名
+
+        # 压缩文件夹
+        zip -q -r "${file}" "$folder"
+        if [ $? -ne 0 ]; then
+            echo "Error: Failed to zip the folder $folder."
+            return 1
+        fi
+
+        # 上传到 OSS
+        oss cp "${file}" "oss://hy-tmp/${file}"
+        if [ $? -eq 0 ]; then
+            echo "Upload successful: ${file}"
+            rm -f "${file}"  # 删除本地压缩文件
+        else
+            echo "Error: Failed to upload ${file} to OSS."
+            return 1
+        fi
+    else
+        echo "Error: Folder $folder does not exist."
+        return 1
     fi
-fi
+}
 
+
+upload "outputs"
 # shutdown
