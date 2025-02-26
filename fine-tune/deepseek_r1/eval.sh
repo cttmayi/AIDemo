ccba1874d0cd8d2846942b802f133d0b98ebe497
ling.yuan <cttmayi@hotmail.com>
2025-02-27 01:00:51 +0800

    update

--- fine-tune/deepseek_r1/eval.sh
+++ fine-tune/deepseek_r1/eval.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-MODEL=deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B
+# MODEL=deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B
+MODEL=~/grpo_saved_lora
 MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,max_model_length=32768,gpu_memory_utilisation=0.8"
 OUTPUT_DIR=data/evals/$MODEL
 
 # AIME 2024
 TASK=aime24
 lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
-    --custom-tasks src/evaluate.py \
+    --custom-tasks eval.py \
     --use-chat-template \
     --output-dir $OUTPUT_DIR
