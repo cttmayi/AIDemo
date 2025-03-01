d03cbb43e2af4596ef472f5758cccfa1f7454cc7
ling.yuan <cttmayi@hotmail.com>
2025-03-02 03:26:36 +0800

    fix

--- fine-tune/deepseek_r1/eval.sh
+++ fine-tune/deepseek_r1/eval.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 # MODEL=deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B
 MODEL="$LOCAL_DIR/temp/Qwen2.5-3B-GRPO"
 # MODEL="$LOCAL_DIR/models/Qwen2.5-3B"
@@ -8,8 +10,12 @@ OUTPUT_DIR=data/evals/$MODEL
 
 # AIME 2024
 TASK=aime24
-lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
-    --custom-tasks fine-tune/deepseek_r1/eval.py \
-    --use-chat-template \
-    --output-dir $OUTPUT_DIR
-    
+#lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
+#    --custom-tasks fine-tune/deepseek_r1/eval.py \
+#    --use-chat-template \
+#    --output-dir $OUTPUT_DIR
+
+
+lighteval vllm \
+    "$SCRIPT_DIR/vllm_model_config.yaml" \
+    "custom|$TASK|0|0"
