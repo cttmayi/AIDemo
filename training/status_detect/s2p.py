1124084d4d5407a3c9f13f4ae7321b1ce0438cd2
ling.yuan <cttmayi@hotmail.com>
2025-06-19 02:52:06 +0800

    update

--- training/status_detect/s2p.py
+++ training/status_detect/s2p.py
@@ -18,9 +18,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 
 
+device = 'cpu'# 'mps' if torch.backends.mps.is_available() else 'cuda' if torch.cuda.is_available() else 'cpu'
+
+print(f'Using device: {device}')
+
 pth = 'model.pth'
 n_status = 10
-n_action = 20
+n_action = 10
 
 def make_dataset(n_status, n_action):
 
@@ -34,11 +38,9 @@ def make_dataset(n_status, n_action):
     # onehot encode the labels(numpy array)
     y = np.eye(n_action)[y]
 
+    print('X = ', X[0].tolist())
+    print('y = ', y[0].tolist())
 
-    print('X = ')
-    print(X[0])
-    print('y = ')
-    print(y[0])
     # 将X的偶数行和奇数行连接起来，形成新的X
     # X = np.concatenate((X[::2], X[1::2]), axis=1)
     # y = np.concatenate((y[::2], y[1::2]), axis=0)
@@ -46,10 +48,10 @@ def make_dataset(n_status, n_action):
     # Train test split
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    X_train = torch.tensor(X_train, dtype=torch.float32)
-    y_train = torch.tensor(y_train, dtype=torch.float32)
-    X_test = torch.tensor(X_test, dtype=torch.float32)
-    y_test = torch.tensor(y_test, dtype=torch.float32)
+    X_train = torch.tensor(X_train, dtype=torch.float32).to(device)
+    y_train = torch.tensor(y_train, dtype=torch.float32).to(device)
+    X_test = torch.tensor(X_test, dtype=torch.float32).to(device)
+    y_test = torch.tensor(y_test, dtype=torch.float32).to(device)
 
     return X_train, y_train, X_test, y_test
 
@@ -60,9 +62,9 @@ class Net(nn.Module):
 
         hidden_dim = 256
         self.status = nn.Sequential(
-            nn.Linear(n_status, hidden_dim*2),
+            nn.Linear(n_status, hidden_dim*10),
             nn.ReLU(),
-            nn.Linear(hidden_dim*2, hidden_dim),
+            nn.Linear(hidden_dim*10, hidden_dim),
             nn.ReLU(),
         )
         self.action = nn.Sequential(
@@ -77,23 +79,25 @@ class Net(nn.Module):
 
 
 def load_model(n_status, n_action, pth):
-    net = Net(n_status=n_status, n_action=n_action)
+    net = Net(n_status=n_status, n_action=n_action).to(device)
     # Load the model if it exists
     try:
         net.load_state_dict(torch.load(pth))
         print(f'Model loaded from {pth}')
     except FileNotFoundError:
         print(f'No model found at {pth}, starting from scratch.')
+    except Exception as e:
+        print(f'Error loading model: {e}, starting from scratch.')
 
     return net
 
 
-def train_model(net, time_steps, X_train, y_train, criterion, optimizer):
+def train_model(net, time_steps, X, y, criterion, optimizer):
     # Train the model
     for epoch in range(time_steps):
         # Forward pass
-        outputs = net(X_train)
-        loss = criterion(outputs, y_train)
+        outputs = net(X)
+        loss = criterion(outputs, y)
         # loss = -torch.mean(torch.sum(y_train*outputs, 1))
 
         # Backward and optimize
@@ -105,11 +109,11 @@ def train_model(net, time_steps, X_train, y_train, criterion, optimizer):
             print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, time_steps, loss.item()))
 
 
-def test_model(net, XX_test, yy_test, threshold=0.5):
+def test_model(net, X, y, threshold=0.5):
     with torch.no_grad():
-        outputs = net(XX_test)
+        outputs = net(X)
         predicted = torch.argmax(outputs.data, dim=1)
-        y_labels = torch.argmax(yy_test, dim=1)
+        y_labels = torch.argmax(y, dim=1)
         # Calculate accuracy
         accuracy = torch.sum(predicted == y_labels).item() / y_labels.size(0)
         print('Test Accuracy 1: {:.2f}%'.format(100 * accuracy))
@@ -127,16 +131,16 @@ X_train, y_train, X_test, y_test = make_dataset(n_status, n_action)
 
 net = load_model(n_status, n_action, pth)
 
-criterion = nn.CrossEntropyLoss()
-# criterion = nn.MSELoss()
-optimizer = optim.Adam(net.parameters(), lr=0.001)
+# criterion = nn.CrossEntropyLoss()
+criterion = nn.MSELoss()
+optimizer = optim.Adam(net.parameters(), lr=0.0001)
 
 # Train the model
-train_model(net, time_steps=200, X_train=X_train, y_train=y_train, criterion=criterion, optimizer=optimizer)
+train_model(net, time_steps=1000, X=X_train, y=y_train, criterion=criterion, optimizer=optimizer)
 
 # Test the model
-test_model(net, X_train, y_train, threshold=0.8)
-test_model(net, X_test, y_test, threshold=0.1)
+test_model(net, X_train, y_train, threshold=0.5)
+test_model(net, X_test, y_test, threshold=0.001)
 
 # Save the model
 torch.save(net.state_dict(), pth)
